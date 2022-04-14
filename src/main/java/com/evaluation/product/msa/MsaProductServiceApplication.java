3dafeedccb9fd2ed6dc77f12a955601e37f02ed0
Limber <vlady200591@gmail.com>
2022-04-14 10:40:07 -0400

    + Proyecto inicial

---
+++ src/main/java/com/evaluation/product/msa/MsaProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.evaluation.product.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaProductServiceApplication.class, args);
+	}
+
+}
